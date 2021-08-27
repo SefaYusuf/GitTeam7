780996a7f3193a705554d267eba4fdfaff2cda52
SefaYusuf <89142294+SefaYusuf@users.noreply.github.com>
2021-08-27 17:42:28 +0200

    versiyon remote

--- src/Ornek.java
+++ src/Ornek.java
@@ -1,5 +1,6 @@
 public class Ornek {
     public static void main(String[] args) {
         System.out.println("Local mainden merhabalar");
+         System.out.println("Remote ornekten merhabalar");
     }
 }
